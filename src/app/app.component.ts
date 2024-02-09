2227c567c457c60a7ddf36382f8fbec3512b6da1
Danny Armijos <daniel2667@hotmail.es>
2024-02-09 22:39:33 +0100

    fin de hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from '../shared/header/header.component';
+import {HeroComponent} from './components/hero/hero.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
