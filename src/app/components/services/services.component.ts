5d500d822750e6d6aaca518e692ca4fc0819a6d0
Danny Armijos <daniel2667@hotmail.es>
2024-02-10 00:17:17 +0100

    fin de services

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TitleCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [],
+  imports: [
+    TitleCasePipe
+  ],
   templateUrl: './services.component.html',
   styles: ``
 })
 export class ServicesComponent {
-
+  itemArray = [
+    {
+      title: 'General Practitioners',
+      url: './assets/icons/services/icon-1.svg'
+    },
+    {
+      title: 'Pregnancy Support',
+      url: './assets/icons/services/icon-2.svg'
+    },
+    {
+      title: 'Nutritional Support',
+      url: './assets/icons/services/icon-3.svg'
+    },
+    {
+      title: 'Pharmaceutical care',
+      url: './assets/icons/services/icon-4.svg'
+    }
+  ]
 }
