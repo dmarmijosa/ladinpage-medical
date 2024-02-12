383f5255f434303fe32c4cd72b0543853f2f331b
Danny Armijos <daniel2667@hotmail.es>
2024-02-12 13:22:37 +0100

    fix bug error calendar input

--- src/app/components/appointment/appointment.component.ts
+++ src/app/components/appointment/appointment.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import {NgbAlert, NgbDateStruct, NgbInputDatepicker, NgbTimepicker, NgbTimeStruct} from '@ng-bootstrap/ng-bootstrap';
+import {FormsModule} from '@angular/forms';
+import {JsonPipe} from '@angular/common';
 
 @Component({
   selector: 'app-appointment',
   standalone: true,
-  imports: [],
+  imports: [
+    NgbInputDatepicker,
+    FormsModule,
+    NgbAlert,
+    JsonPipe,
+    NgbTimepicker
+  ],
   templateUrl: './appointment.component.html',
   styles: ``
 })
